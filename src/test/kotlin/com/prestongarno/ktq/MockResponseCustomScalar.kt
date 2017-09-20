a7fd42ba5a9392ae6e51f01e85dc73204d03af65
preston <prestongarno@gmail.com>
2017-09-20 14:25:26 -0400

    tested more custom scalar mappings

--- src/test/kotlin/com/prestongarno/ktq/MockResponseCustomScalar.kt
+++ src/test/kotlin/com/prestongarno/ktq/MockResponseCustomScalar.kt
@@ -2,8 +2,10 @@ package com.prestongarno.ktq
 
 import com.prestongarno.ktq.QSchemaType.QCustomScalar
 import com.prestongarno.ktq.adapters.custom.StringScalarMapper
+import com.google.common.truth.Truth.assertThat
 import org.intellij.lang.annotations.Language
 import org.junit.Test
+import java.io.File
 import java.time.Instant
 import java.util.Calendar
 import java.util.Date
@@ -27,10 +29,29 @@ class MockResponseCustomScalar {
       assertTrue(Calendar.getInstance().time.after(dateCreated))
     }
   }
+
+  @Test fun singleCustomScalarField2() {
+    val myNote = object : QModel<Note>(Note) {
+      val webUrl by model.webUrl.init(StringScalarMapper { File(it).toURI() })
+    }
+
+    @Language("JSON") val response = """{
+        "webUrl": "/dev/null"
+      }"""
+
+    myNote.run {
+      onResponse(response)
+      assertThat(webUrl.path).isEqualTo("/dev/null")
+    }
+  }
 }
 
 object DateTime : CustomScalar
 
+object URI : CustomScalar
+
 object Note : QSchemaType {
   val dateCreated: CustomScalarInitStub<DateTime> by QCustomScalar.stub()
+
+  val webUrl: CustomScalarInitStub<URI> by QCustomScalar.stub()
 }
