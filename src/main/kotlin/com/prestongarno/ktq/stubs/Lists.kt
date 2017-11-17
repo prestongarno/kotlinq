bac8857a870eacff4daf76b30b9cae6624934e37
Preston Garno <prestongarno@gmail.com>
2017-11-17 14:03:09 -0600

    Added custom scalar list interfaces and implementations

--- src/main/kotlin/com/prestongarno/ktq/stubs/Lists.kt
+++ src/main/kotlin/com/prestongarno/ktq/stubs/Lists.kt
@@ -1,19 +1,7 @@
 package com.prestongarno.ktq.stubs
 
-import com.prestongarno.ktq.ArgBuilder
-import com.prestongarno.ktq.CustomScalar
 import com.prestongarno.ktq.QModel
-import com.prestongarno.ktq.QType
-import com.prestongarno.ktq.SchemaStub
-import com.prestongarno.ktq.adapters.custom.QScalarListMapper
 import com.prestongarno.ktq.DelegateProvider
 
 interface UnionListStub : DelegateProvider<List<QModel<*>>>
 
-interface CustomScalarListStub<U: QScalarListMapper<T>, out T> : DelegateProvider<List<T>>
-interface CustomScalarListInitStub<T: CustomScalar> : SchemaStub {
-  fun <U: QScalarListMapper<A>, A> querying(of: U): CustomScalarListStub<U, A>
-}
-interface CustomScalarListConfigStub<T: CustomScalar, out A: ArgBuilder> : SchemaStub {
-  fun config(provider: A.() -> Unit): CustomScalarListInitStub<T>
-}
