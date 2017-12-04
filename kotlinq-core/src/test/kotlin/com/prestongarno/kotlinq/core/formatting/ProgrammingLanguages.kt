c48138192495d74e71656d5e15154a3496c9712d
Preston Garno <prestongarno@gmail.com>
2017-12-03 23:16:46 -0600

    Test suite for sample .graphqls (#76)
    * renamed test packages
    
    * pretty printing working to Nth level of nesting
    
    * Removed old tests from last API version
    
    * renamed test packages
    
    * pretty printing working to Nth level of nesting
    
    * Removed old tests from last API version
    
    * Fixed broken test from refactor
    
    * fixed duplicate import
    
    * Using maven local install for stub gw build for test API project
    
    * recovered Antlr grammar rules file
    
    * removed node test resources
    
    * re-added node submodule
    
    * rm submodule
    
    * removed duplicate gitconfig sub entry
    
    * Fixed union return type compile issue (#74)
    
    * First test case from stub module classes
    
    * QModel.toGraphQL() tests on com.language mock API
    
    * doc
    
    * println debugging
    
    * docs, formatting, fixed import breaking build

--- kotlinq-core/src/test/kotlin/com/prestongarno/kotlinq/core/formatting/ProgrammingLanguages.kt
+++ kotlinq-core/src/test/kotlin/com/prestongarno/kotlinq/core/formatting/ProgrammingLanguages.kt
@@ -17,11 +17,274 @@
 
 package com.prestongarno.kotlinq.core.formatting
 
+import com.google.common.truth.Truth.assertThat
+import com.languages.Architecture
+import com.languages.Date
+import com.languages.Feature
+import com.languages.Framework
+import com.languages.OperatingSystem
 import com.languages.ProgrammingLanguage
 import com.languages.Query
+import com.languages.SoftwareComponent
 import com.prestongarno.kotlinq.core.QModel
+import com.prestongarno.kotlinq.core.adapters.custom.StringScalarMapper
+import com.prestongarno.kotlinq.core.getFragments
+import org.junit.Test
+import java.time.Instant
 
-class ProgrammingLanguagesQuery : QModel<Query>(Query) {
-  val query by model.search(Query.SearchArgs("", 10)) {
+
+/**
+ * Four simple test cases using the kolinq-core library which covers
+ * the front end of a GraphQL client: making requests.
+ *
+ * Using classes from the (build-time generated) test module package [com.languages]
+ *
+ * Implemented subclasses for making queries are at the end of the file
+ *
+ * GraphQL SDL file is in the kotlinq-test-api module resources folder
+ *
+ * @author prestongarno
+ */
+class TechSchemaQueryTests {
+
+  @Test fun `query fragment on programming lang type`() {
+
+    val query = TechnologyQuery(searchTerm = "kotlin", limit = 9000) {
+      onProgrammingLanguage(::Language)
+    }
+
+    val result = query.toGraphql(pretty = true)
+
+    val expected = """
+      |{
+      |  search(term: \"kotlin\",limit: 9000) {
+      |    ... on ProgrammingLanguage {
+      |      name
+      |      primaryFeatures {
+      |        name
+      |        description
+      |      }
+      |      isTyped
+      |      since
+      |    }
+      |  }
+      |}
+      """.trimMargin("|")
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @Test fun `query on operating system`() {
+
+    val query = TechnologyQuery(searchTerm = "Windows", limit = 69) {
+      onOperatingSystem(::OperatingSys)
+    }
+
+    val result = query.toGraphql(pretty = true)
+
+    val expect = """
+      |{
+      |  search(term: \"Windows\",limit: 69) {
+      |    ... on OperatingSystem {
+      |      name
+      |      since
+      |      archSupport
+      |    }
+      |  }
+      |}
+      """.trimMargin("|")
+  }
+
+  @Test fun `query fragment programming lang and operating system`() {
+
+    val query = TechnologyQuery(searchTerm = "unix", limit = 35) {
+      onProgrammingLanguage(::Language)
+      onOperatingSystem(::OperatingSys)
+    }
+
+    val result = query.toGraphql(pretty = true)
+
+    val expect = """
+      |{
+      |  search(term: \"unix\",limit: 35) {
+      |    ... on ProgrammingLanguage {
+      |      name
+      |      primaryFeatures {
+      |        name
+      |        description
+      |      }
+      |      isTyped
+      |      since
+      |    }
+      |    ... on OperatingSystem {
+      |      name
+      |      since
+      |      archSupport
+      |    }
+      |  }
+      |}
+      """.trimMargin("|")
+
+    assertThat(result).isEqualTo(expect)
+  }
+
+  @Test fun `nested fragment multiple levels on framework`() {
+
+
+    val query = TechnologyQuery(searchTerm = "python") {
+      onFramework {
+        FrameworkModel {
+          onProgrammingLanguage(::Language)
+          onOperatingSystem(::OperatingSys)
+          onFramework(::NestedFrameworkModel)
+        }
+      }
+    }
+
+    val result = query.toGraphql(pretty = true)
+
+    // 5 fragments in query, but 2 are exactly equal
+    assertThat(query.getFragments().size)
+        .isEqualTo(4)
+
+    val expect = """
+      |{
+      |  search(term: \"python\",limit: 10) {
+      |    ... on Framework {
+      |      name
+      |      languagesUsed {
+      |        name
+      |        primaryFeatures {
+      |          name
+      |          description
+      |        }
+      |        isTyped
+      |        since
+      |      }
+      |      dependencies {
+      |        ... on ProgrammingLanguage {
+      |          name
+      |          primaryFeatures {
+      |            name
+      |            description
+      |          }
+      |          isTyped
+      |          since
+      |        }
+      |        ... on OperatingSystem {
+      |          name
+      |          since
+      |          archSupport
+      |        }
+      |        ... on Framework {
+      |          name
+      |          platform {
+      |            ... on ProgrammingLanguage {
+      |              name
+      |              primaryFeatures {
+      |                name
+      |                description
+      |              }
+      |              isTyped
+      |              since
+      |            }
+      |            ... on OperatingSystem {
+      |              name
+      |              since
+      |              archSupport
+      |            }
+      |          }
+      |          primaryFeatures {
+      |            name
+      |            description
+      |          }
+      |        }
+      |      }
+      |    }
+      |  }
+      |}
+      """.trimMargin("|")
+  }
+
+  // non-recursive nested fragment for ^^^ test to prevent cycle
+  // also non-local class because kotlin class fqnames can't be escaped
+  class NestedFrameworkModel : QModel<Framework>(Framework) {
+    val name by model.name
+    val platform by model.platform {
+      on(::Language)
+      on(::OperatingSys)
+    }
+    val features by model.primaryFeatures.query(::FeatureModel)
+  }
+
+}
+
+/************************************************************
+ *
+ * Mock Implementation classes covering basic use cases of graphql spec
+ *
+ ***********************************************************/
+
+class TechnologyQuery(
+    searchTerm: String = "kotlin",
+    limit: Int = 10,
+    block: SoftwareComponent.() -> Unit
+) : QModel<Query>(Query) {
+
+  val query: QModel<*>? by model.search(Query.SearchArgs(searchTerm, limit)) {
+    fragment(block)
+  }
+
+}
+
+
+class Language : QModel<ProgrammingLanguage>(ProgrammingLanguage) {
+
+  val name: String by model.name
+
+  val features: List<FeatureModel> by model.primaryFeatures.query(::FeatureModel)
+
+  val isTyped: Boolean by model.isTyped
+
+  // Custopm deserializing from graphql fields
+  val since: java.util.Date by model.since.map(StringScalarMapper(
+      Date::parseFromQuery))
+
+}
+
+class OperatingSys : QModel<OperatingSystem>(OperatingSystem) {
+
+  val name: String by model.name
+
+  val since: java.util.Date by model.since.map(StringScalarMapper(
+      Date::parseFromQuery))
+
+  val architecture by model.archSupport { default = Architecture.X86_64 }
+
+}
+
+class FrameworkModel(
+    dependencyScope: SoftwareComponent.() -> Unit
+) : QModel<Framework>(Framework) {
+
+  val name: String by model.name
+
+  val languagesUsed: List<Language> by model.languagesUsed.query(::Language)
+
+  val dependencies: List<QModel<*>> by model.dependencies {
+    fragment(dependencyScope)
   }
 }
+
+
+class FeatureModel : QModel<Feature>(Feature) {
+
+  val name: String by model.name
+
+  val description: String by model.description
+
+}
+
+fun Date.parseFromQuery(value: String): java.util.Date =
+    java.util.Date.from(Instant.parse(value))
+
