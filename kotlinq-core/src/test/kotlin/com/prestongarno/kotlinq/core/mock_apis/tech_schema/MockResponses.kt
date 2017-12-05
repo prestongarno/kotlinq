d048a721795e64b2369984d7b9e6636ae0fad284
Preston Garno <prestongarno@gmail.com>
2017-12-04 23:05:57 -0600

    removed un-reliable time test requirement

--- kotlinq-core/src/test/kotlin/com/prestongarno/kotlinq/core/mock_apis/tech_schema/MockResponses.kt
+++ kotlinq-core/src/test/kotlin/com/prestongarno/kotlinq/core/mock_apis/tech_schema/MockResponses.kt
@@ -68,8 +68,6 @@ class MockResponses {
         time = this@apply.since
         get(Calendar.DAY_OF_MONTH) eq 19
         get(Calendar.MONTH) eq 1
-        // literally have no clue how this date works
-        get(Calendar.HOUR_OF_DAY) eq 16
       }
     }
 
