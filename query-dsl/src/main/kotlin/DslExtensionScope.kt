37efc66a86b2c4025cf857247148ba1961125c84
Preston Garno <prestongarno@gmail.com>
2018-02-17 21:22:17 -0500

    DSL technical outline

--- query-dsl/src/main/kotlin/DslExtensionScope.kt
+++ query-dsl/src/main/kotlin/DslExtensionScope.kt
@@ -4,6 +4,8 @@ import TypeDefinition
 import org.kotlinq.api.GraphQlInstance
 import kotlin.reflect.KProperty0
 import kotlin.reflect.KProperty1
+import kotlin.reflect.KFunction0
+import org.kotlinq.dsl.fields.FreeProperty
 
 typealias ScalarOp = KProperty0<KProperty1<DslExtensionScope, Scalar>>
 
@@ -49,9 +51,16 @@ typealias ScalarOp = KProperty0<KProperty1<DslExtensionScope, Scalar>>
  *
  * Details:
  *
- *   1. KCallable0<Scalar> properties "string", "int", "float", and "boolean"
- *   2. Local scoped extension *operator* function "String.invoke(KCallable0<Scalar>): (GraphQlInstance) -> FreeProperty"
- *   3. Some other complex higher-order stuff which is currently working in the [Fooo] class
+ *   1. Local scoped extension functions "String.\[integer, boolean, string, etc...\]: (GraphQlInstance) -> FreeProperty"
+ *   2. [KFunction0]<([GraphQlInstance]) -> [FreeProperty]> is the type when expressing "stringLiteral::string" etc.
+ *     - ^ is a DSL expression for a *dynamic name* with *scalar type*
+ *   3. Local operator overload operator function [KFunction0]<([GraphQlInstance]) -> [FreeProperty]>.not()
+ *     - Nested higher-order type *extension* function definitions is the key to this
+ *     - the [KFunction0] type guarantees direct invocation, and the returned lambda type is invoked with the current instance's context passed ad the argument, which returns the name + type information.
+ *     - This is done from our controlled [GraphBuilder] scope on calling the operator functions ***not*** and ***unaryMinus***.
+ *   4. ***not*** operator overload represents *non-nullability*
+ *   5. ***unaryMinus*** operator overload represents field *nullability*
+ *   6. Both operators are required because provide an instance-bound frame of reference to create the resulting [org.kotlinq.api.Adapter] property from property meta information
  */
 interface DslExtensionScope : GraphQlInstance {
 
