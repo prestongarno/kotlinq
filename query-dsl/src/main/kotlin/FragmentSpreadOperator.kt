d326dd154493edcf37f3f1f59bf8bd4efdcecb68
Preston Garno <prestongarno@gmail.com>
2018-02-17 21:02:34 -0500

    cleaned working name->type->property DSL example

--- query-dsl/src/main/kotlin/FragmentSpreadOperator.kt
+++
@@ -1,3 +0,0 @@
-package org.kotlinq.dsl
-
-interface FragmentSpreadOperator
---
+++ query-dsl/src/main/kotlin/fields/FreeProperty.kt
@@ -0,0 +1,11 @@
+package fields
+
+/**
+ * A [FreeProperty] is a DSL concept which can be compared to
+ * an "unbound" callable (kotlin) or a java lambda before it is
+ * "bound" or "linked" to a callsite before executing.
+ *
+ * This class simply contains information about a query field and
+ * arguments. It is the target of many DSL extension functions and properties.
+ */
+data class FreeProperty(val name: String, val arguments: Map<String, Any>)
