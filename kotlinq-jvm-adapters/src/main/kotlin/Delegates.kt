3ab5c43e5b480589c3a16e122ba999334d7416c8
Preston Garno <prestongarno@gmail.com>
2018-02-25 13:32:05 -0500

    init

---
+++ kotlinq-jvm-adapters/src/main/kotlin/Delegates.kt
@@ -0,0 +1,26 @@
+package org.kotlinq.api.schema
+
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+
+private
+class NotNullDelegate<out T>(val map: Map<String, Any?>) : ReadOnlyProperty<Any?, T> {
+  @Suppress("UNCHECKED_CAST") override
+  operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+      map[property.name] as? T ?: throw NullPointerException()
+}
+
+class NullableDelegate<out T>(val map: Map<String, Any?>) : ReadOnlyProperty<Any?, T?> {
+  @Suppress("UNCHECKED_CAST") override
+  operator fun getValue(thisRef: Any?, property: KProperty<*>): T? = map[property.name] as? T
+}
+
+
+fun <T> Map<String, Any?>.notNull(): ReadOnlyProperty<Any?, T> = NotNullDelegate(this)
+
+fun <T> Map<String, Any?>.nullable(): ReadOnlyProperty<Any?, T> = NotNullDelegate(this)
+
+class Foo(map: Map<String, Any>) {
+  val foo: String by map.notNull()
+}
