af8a6a4577fdeaf3d6faa78408a511c5de4706be
Preston Garno <prestongarno@gmail.com>
2018-02-27 15:55:43 -0500

    nested fragment spreads

--- kotlinq-jvm-adapters/src/main/kotlin/TypedFragmentScope.kt
+++ kotlinq-jvm-adapters/src/main/kotlin/TypedFragmentScope.kt
@@ -21,8 +21,20 @@ class TypedFragmentScope<T : Data?> internal constructor(private val bindableCon
    * Enables arguments on fields while using generic DSL
    */
   @Suppress("UNCHECKED_CAST")
-  operator fun KProperty1<T, *>.invoke(arguments: Map<String, Any>) {
-    bindScalarPropertyIfApplicable(this, arguments)
+  operator fun <R> KProperty1<T, R>.invoke(arguments: Map<String, Any>)
+      : FragmentField<T, R> =
+      bindScalarPropertyIfApplicable(this, arguments) ?: FragmentField(this)
+
+  infix fun KProperty1<T, Data?>.union(block: InterfaceFragmentSpreadScope<Data?>.() -> Unit) {
+    buildFromFragmentScope(this, block)
+  }
+
+  operator fun <R : Data?> FragmentField<T, R>.rangeTo(block: InterfaceFragmentSpreadScope<R>.() -> Unit) {
+    buildFromFragmentScope(property, block)
+  }
+
+  infix fun FragmentField<T, Data?>.union(block: InterfaceFragmentSpreadScope<Data?>.() -> Unit) {
+    buildFromFragmentScope(property, block)
   }
 
   inline operator fun <reified X : Data?> KProperty1<T, X>.rangeTo(
@@ -30,23 +42,20 @@ class TypedFragmentScope<T : Data?> internal constructor(private val bindableCon
       buildFromFragmentScope(this, block)
 
 
-
   @PublishedApi internal
-  fun bindScalarPropertyIfApplicable(prop: KProperty1<T, *>, arguments: Map<String, Any>) {
-    val kind = prop.returnType.scalarKind() ?: return
+  fun <R> bindScalarPropertyIfApplicable(prop: KProperty1<T, *>, arguments: Map<String, Any>): FragmentField<T, R>? {
+    val kind = prop.returnType.scalarKind() ?: return null
     PropertyInfo.propertyNamed(prop.name)
         .typeKind(kind)
         .arguments(arguments)
         .build()
         .let(Kotlinq.adapterService.scalarAdapters::newAdapter)
         .let(bindableContext::register)
+    return null
   }
 
   @PublishedApi internal
-  fun <X : Data?> buildFromFragmentScope(
-      property: KProperty1<T, X>,
-      block: InterfaceFragmentSpreadScope<X>.() -> Unit
-  ) {
+  fun <X : Data?> buildFromFragmentScope(property: KProperty1<T, X>, block: InterfaceFragmentSpreadScope<X>.() -> Unit) {
     val kind = property.returnType.dataKind() ?: return
     PropertyInfo.propertyNamed(property.name)
         .typeKind(kind)
