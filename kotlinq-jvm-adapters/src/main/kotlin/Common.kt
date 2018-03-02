3d291fa91e8eebacca49585ef4b3e3ecdd272370
Preston Garno <prestongarno@gmail.com>
2018-03-01 23:53:17 -0500

    Kind check tests

--- kotlinq-jvm-adapters/src/main/kotlin/Common.kt
+++ kotlinq-jvm-adapters/src/main/kotlin/Common.kt
@@ -8,3 +8,8 @@ fun <T> use(value: T, block: T.() -> Unit) = value.apply(block)
 fun <T : Any, R : Any, Z> T.useWith(value: R, block: T.(R) -> Z): Z = this.block(value)
 
 fun Any?.ignore() = Unit
+
+fun <T> List<T>.contains(predicate: (T) -> Boolean) = find(predicate) != null
+
+
+fun <T> List<T>.none(predicate: (T) -> Boolean) = find(predicate) == null
